db45b44392d1bc2c164c67a28c01bddf8180a525
Reagan Hoang <reagang4@users.noreply.github.com>
2020-04-22 15:24:09 -0700

    set up nav

---
+++ components/MainComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View, Platform  } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+class Main extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <DirectoryNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
