249f2e3a2706d37991cdd5cc2d6e8d5a7e610cc3
Reagan Hoang <reagang4@users.noreply.github.com>
2020-05-05 15:37:45 -0700

    add animation

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 export default class Contact extends Component {
 
@@ -11,19 +12,21 @@ export default class Contact extends Component {
     render() {
         return(
             <ScrollView>
-                <Card 
-                title="Contact Information"
-                wrapperStyle={{ margin: 20 }} >
-                    <Text style={{marginBottom: 10}}> 
-                        1 Nucamp Way {"\n"}
-                        Seattle, WA 98001 {"\n"}
-                        U.S.A. 
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card 
+                    title="Contact Information"
+                    wrapperStyle={{ margin: 20 }} >
+                        <Text style={{marginBottom: 10}}> 
+                            1 Nucamp Way {"\n"}
+                            Seattle, WA 98001 {"\n"}
+                            U.S.A. 
+                            </Text>
+                            <Text>
+                            Phone: 1-206-555-1234 {"\n"}
+                            Email: campsites@nucamp.co
                         </Text>
-                        <Text>
-                        Phone: 1-206-555-1234 {"\n"}
-                        Email: campsites@nucamp.co
-                    </Text>
-                </Card>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
