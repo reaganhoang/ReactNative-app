84823df114b0180a05b689dc585d48d363b93dc6
Reagan Hoang <reagang4@users.noreply.github.com>
2020-05-16 02:20:55 -0700

    add more async function

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -343,21 +343,26 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
+        this.showNetInfo();
+        // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        //     this.handleConnectivityChange(connectionInfo);
+        // });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeNetInfo();
+    }
+
+    async showNetInfo(){
+         await NetInfo.fetch().then(connectionInfo => {
             (Platform.OS === 'ios') ?
                 Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
                 : ToastAndroid.show('Initial Network Connectivity Type: ' +
                     connectionInfo.type, ToastAndroid.LONG);
         });
 
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
     }
 
-    componentWillUnmount() {
-        this.unsubscribeNetInfo();
-    }
 
     handleConnectivityChange = connectionInfo => {
         let connectionMsg = 'You are now connected to an active network.';
